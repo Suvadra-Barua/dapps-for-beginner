c634c055ad18c40bc3362cea507b09b8c5da480c
Suvadra-Barua <suvadra@universalmachine.io>
2023-11-02 20:22:32 +0600

    Add campaign creation components

--- web3-crowdfunding/src/App.jsx
+++ web3-crowdfunding/src/App.jsx
@@ -1,12 +1,14 @@
 import { ChakraProvider, extendTheme, CSSReset, Box, Text } from '@chakra-ui/react';
 import customTheme from './theme';
 import Navbar from './components/navbar';
+import CampaignCreation from './components/createCampaign';
 
 function App() {
   return (
     <ChakraProvider theme={customTheme}>
       <CSSReset />
       <Navbar />
+      <CampaignCreation />
       <Box p={8}>
         <Text fontSize="xl">Dark Cool Background</Text>
       </Box>
